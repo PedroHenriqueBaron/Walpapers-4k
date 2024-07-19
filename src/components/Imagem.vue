3f65a58c2eba8ddc3714c292ca36bd8a211e2f16
PedroHenriqueBaron <161463685+PedroHenriqueBaron@users.noreply.github.com>
2024-07-19 11:56:45 -0300

    Add files via upload

---
+++ src/components/Imagem.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="imagem1">
+            <img src="img/cachorros-engracados.jpg" height="17%" width="100%" />
+        </div>
+        <br>
+        <div class="imagem2">
+            <img src="img/mulherpeixe.jpg" height="37%" width="64%" />
+            <img src="img/9d5a18e7ffc7e7afba8d609d6e21f5c089772175_00.jpg" height="37%" width="35%" />
+        </div>
+</template>
+<script>
+    export default {
+        name: 'Imagem'
+    }
+</script>
---
+++ src/components/PaginaPrincipal.vue
@@ -0,0 +1,29 @@
+<template>
+    <!DOCTYPE html>
+
+    <head>
+        <title> Walpapers 4k </title>
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+
+    <body>
+        <div class="titulo">
+            <h1>Walpapers 4k</h1>
+            <h2>Pedro Baron</h2>
+        </div>
+        <!-- <div class="imagem1">
+            <img src="img/cachorros-engracados.jpg" height="17%" width="100%" />
+        </div>
+        <br>
+        <div class="imagem2">
+            <img src="img/mulherpeixe.jpg" height="37%" width="64%" />
+            <img src="img/9d5a18e7ffc7e7afba8d609d6e21f5c089772175_00.jpg" height="37%" width="35%" />
+        </div> -->
+        <div class="video">
+            <video width="100%" height="7%" controls>
+                <source src="img/CINCO PATINHOS SOUTH AMERICA MEMES.mp4">
+            </video>
+        </div>
+    </body>
+
+</html></template>
