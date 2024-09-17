a8d6d3f94438051542cdf24638c651566dcbae49
PedroHenriqueBaron <161463685+PedroHenriqueBaron@users.noreply.github.com>
2024-09-17 10:08:54 -0300

    Add files via upload

--- src/components/Imagem.vue
+++ src/components/Imagem.vue
@@ -7,7 +7,7 @@
         <img src="img/mulherpeixe.jpg" height="37%" width="64%" />
         <img src="img/9d5a18e7ffc7e7afba8d609d6e21f5c089772175_00.jpg" height="37%" width="35%" />
     </div>
-    <img src="img/8k.jpg"/>
+    <img src="img/8k.webp"/>
 </template>
 <script>
 export default {
--- src/components/ImagemResolucao.vue
+++ src/components/ImagemResolucao.vue
@@ -1,3 +1,4 @@
+<!-- Transformei a imagem de 4mg .jpg em .webp para ser mais veloz em seu carregamento -->
 <template>
-    <img src="@/assets/8k.jpg" height="17%" width="100%" />
+    <img src="@/assets/8k.webp" height="17%" width="100%" />
 </template>
